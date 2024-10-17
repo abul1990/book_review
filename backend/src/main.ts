8eaeab7fac3054e831f6dd426cc400349588cc0c
abul1990 <abulassan@gmail.com>
2024-10-17 19:43:56 +0400

    API | Configure allowed origins env variable

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,16 +3,16 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const port = process.env.PORT || 4000;
+  const port = process.env.PORT || 8080;
   
   app.enableCors({
-    origin: '*',
+    origin: process.env.ALLOWED_ORIGIN || 'http://localhost:3000',
     credentials: true,
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     allowedHeaders: 'Content-Type, Accept, Authorization',
   });
 
   await app.listen(port);
-  console.log(`Application is running on: http://localhost:${port}`);
+  console.log(`Application is running on port: ${port}`);
 }
 bootstrap();
