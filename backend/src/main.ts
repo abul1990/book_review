5b63cb4d0fa6b4994668c1e03b69d324d7dc768f
abul1990 <101988743+abul1990@users.noreply.github.com>
2024-10-17 12:41:36 +0400

    Add Port environment variable

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,14 +3,16 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const port = process.env.PORT || 4000;
   
   app.enableCors({
-    origin: 'http://localhost:3000',
+    origin: '*',
     credentials: true,
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     allowedHeaders: 'Content-Type, Accept, Authorization',
   });
 
-  await app.listen(8080);
+  await app.listen(port);
+  console.log(`Application is running on: http://localhost:${port}`);
 }
 bootstrap();
